fbb57ca40ce60cc60f118bd957850bc55f6bbd53
dr-guangtou <dr.guangtou@gmail.com>
2017-11-26 14:54:02 -0800

    Add distance_to() method; and placeholder for is_region() method

--- unagi/object.py
+++ unagi/object.py
@@ -37,11 +37,84 @@ class HscObject(object):
     def icrs(self):
         return self._sky_coord()
 
+    @property
+    def galactic(self):
+        return self.icrs.galactic
+
+    @property
+    def galactic_l(self):
+        return self.galactic.l.value
+
+    @property
+    def galactic_b(self):
+        return self.galactic.b.value
+
+    def distance_to(self, *args, **kwargs):
+        """Get the angular distance to another object.
+
+        Examples
+        --------
+
+            >>> object_1 = HscObject(30.0, -2.0)
+            >>> object_2 = HscObject(31.0, -1.9)
+            >>> ang_sep = object_1.distance_to(object_2)
+
+        Or:
+            >>> coord_2 = SkyCoord(31.0, -1.9, unit='deg', frame='icrs')
+            >>> ang_sep = object_1.distance_to(coord_2)
+
+        Or:
+            >>> ang_sep = object_1.distance_to(ra=30.0, dec=-1.9)
+            >>> ang_sep = object_1.distance_to(l=100.0, b=20.0)
+
+        Parameters
+        ----------
+            another_object : object (either HscObject or SkyCoord)
+                Another HSC object or a SkyCoord object.
+            ra, dec : float (optional)
+                ICRS coordinate of an object.
+            l, b : float (optional)
+                Galactic coordinate of another object.
+        """
+        if len(args) > 0:
+            if isinstance(args[0], HscObject):
+                return self.icrs.separation(args[0].icrs)
+            elif isinstance(args[0], SkyCoord):
+                return self.icrs.separation(args[0])
+            else:
+                raise TypeError("Wrong type of object ",
+                                "Should be HscObject or SkyCoord")
+        elif ('ra' in kwargs) and ('dec' in kwargs):
+            return self.icrs.separation(SkyCoord(kwargs['ra'],
+                                                 kwargs['dec'],
+                                                 unit="deg",
+                                                 frame=self.frame))
+        elif ('l' in kwargs) and ('b' in kwargs):
+            return self.icrs.separation(SkyCoord(kwargs['l'], kwargs['b'],
+                                                 unit="deg",
+                                                 frame="galactic").icrs)
+        else:
+            raise Exception("Something wrong !")
+
+    def in_region(self, *args):
+        """Whether the object is within specific region.
+        Examples
+        --------
+
+            >>> object_1 = HscObject(30.0, -2.0)
+            >>> object_1.in_region(Tract)
+
+        Parameters
+        ----------
+        """
+        raise NotImplementedError
+
     def _sky_coord(self):
         """
         Create a SkyCoord object.
         """
-        coord = SkyCoord(self.ra, self.dec, unit="deg", frame=self.frame)
+        coord = SkyCoord(self.ra, self.dec, unit="deg",
+                         frame=self.frame)
         if self.frame is not 'icrs':
             return coord.icrs
         else:
