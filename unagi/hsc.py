8bb2d2f18e0d59426770c11519f5e102dbab3dfd
dr-guangtou <dr.guangtou@gmail.com>
2019-05-13 14:08:50 -0700

    Add function to download image

--- unagi/hsc.py
+++ unagi/hsc.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 """Core functions"""
 
+import os
 import urllib
+import shutil
 import warnings
 
 import astropy.units as u
 from astropy.io import fits
+from astropy.utils.data import download_file
 
 from . import config
 
@@ -24,6 +27,10 @@ DEFAULT_CUTOUT_ORIGIN = {
     'filter': 'HSC-I', 'rerun': ''
 }
 
+class HscException(Exception):
+    """Class for error related to data release information.
+    """
+    pass
 
 class Hsc():
     """
@@ -89,28 +96,60 @@ class Hsc():
         password_mgr.add_password(None, self.archive.base_url, username, password)
         handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
 
-        # create "opener" (OpenerDirector instance)
-        opener = urllib.request.build_opener(handler)
+        # Create "opener" (OpenerDirector instance)
+        self.opener = urllib.request.build_opener(handler)
 
         try:
-            # use the opener to fetch a URL
-            opener.open(self.archive.base_url)
             # Install the opener.
-            urllib.request.install_opener(opener)
+            self.opener.open(self.archive.base_url)
+            urllib.request.install_opener(self.opener)
             self.is_login = True
-            self.opener = opener
         except urllib.error.HTTPError as e:
             print("! Can not login to HSC archive: %s" % str(e))
             self.opener = None
 
-    def download_cutout(self):
+    def logout(self):
+        """
+        Log out of the HSC server.
+
+        TODO: This actually does not do anything.
+        """
+        if not self.is_login:
+            print("# Has not login to HSC archive yet!")
+        else:
+            print("# Log out of HSC archive now!")
+            self.opener = None
+            self.is_login = False
+
+    def download_cutout(self, coord, output_file, coord_2=None, w_half=None, h_half=None,
+                        filt='HSC-I', img_type='coadd', image=True, variance=False, mask=False,
+                        overwrite=True):
         """
         Download coadded or warped cutout image(s).
+
+        Parameters:
+        -----------
         """
-        pass 
+        cutout_kwargs = {'coord_2': coord_2, 'w_half': w_half, 'h_half': h_half,
+                         'filt': filt, 'img_type': img_type, 'image': image,
+                         'variance': variance, 'mask': mask}
+
+        cutout = self.get_cutout_image(coord, **cutout_kwargs)
+
+        if img_type == 'coadd':
+            # Download FITS file for coadd image.
+            _ = cutout.writeto(output_file, overwrite=overwrite)
+        elif img_type == 'wrap':
+            # Download the tarball for wrapped images.
+            if os.path.isfile(output_file) and not overwrite:
+                raise HscException("# File {} exists!".format(output_file))
+            else:
+                _ = shutil.move(download_file(cutout, show_progress=False), output_file)
+        else:
+            raise HscException("# Wrong image type: coadd or wrap !")
 
     def get_cutout_image(self, coord, coord_2=None, w_half=None, h_half=None, filt='HSC-I',
-                         img_type='coadd', image=True, variance=False, mask=False):
+                         img_type='coadd', image=True, variance=False, mask=False, verbose=False):
         """
         Get HSC cutout image.
 
@@ -124,10 +163,13 @@ class Hsc():
             coord, coord_2=coord_2, w_half=w_half, h_half=h_half, **cutout_kwargs)
 
         if img_type == 'warp':
-            warnings.warn("# Not a coadd cutout, will return the url")
+            if verbose:
+                warnings.warn("# Not a coadd cutout, will return the url")
             return cutout_url
 
         try:
+            if verbose:
+                print("# Downloading FITS image from {}".format(cutout_url))
             cutout = fits.open(cutout_url)
         except urllib.error.HTTPError as e:
             print("# Error message: {}".format(e))
