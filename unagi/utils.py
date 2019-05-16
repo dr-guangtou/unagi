319aa106ac04ff9ae9b23c3ba9c1d76fe962f715
dr-guangtou <dr.guangtou@gmail.com>
2019-05-16 13:49:27 -0700

    Allow more flexible name of the keys

--- unagi/utils.py
+++ unagi/utils.py
@@ -72,12 +72,17 @@ def r_phy_to_ang(r_phy, redshift, cosmo=None, phy_unit='kpc', ang_unit='arcsec')
     return (r_phy / cosmo.kpc_proper_per_arcmin(redshift)).to(u.Unit(ang_unit))
 
 
-def stats_summary(X, sigma=5.0, n_min=10, kde=True, bw=None):
+def stats_summary(X, sigma=5.0, n_min=10, kde=True, bw=None, prefix=None):
     """
     Statistical summary of an array.
     """
-    summary = {'low': np.nan, 'upp': np.nan, 'mean': np.nan,
-               'median': np.nan, 'std': np.nan, 'kde': None, 'sigma': sigma}
+    keys = ['low', 'upp', 'mean', 'median', 'std', 'kde', 'sigmaclip']
+    if prefix is not None:
+        keys = ['_'.join([prefix, key]) for key in keys]
+
+    summary = {keys[0]: np.nan, keys[1]: np.nan, keys[2]: np.nan,
+               keys[3]: np.nan, keys[4]: np.nan, keys[5]: None,
+               keys[6]: sigma}
 
     # Only use the ones with a good flux
     flag = np.isfinite(X)
@@ -90,25 +95,27 @@ def stats_summary(X, sigma=5.0, n_min=10, kde=True, bw=None):
     # Sigma clipping
     if sigma is not None and sigma > 0:
         X_clipped, X_low, X_upp = sigmaclip(X, low=sigma, high=sigma)
-        summary['low'] = X_low
-        summary['upp'] = X_upp
+        summary[keys[0]] = X_low
+        summary[keys[1]] = X_upp
     else:
         X_clipped = X
-        summary['low'] = np.nanmin(X)
-        summary['upp'] = np.nanmax(X)
+        summary[keys[0]] = np.nanmin(X)
+        summary[keys[1]] = np.nanmax(X)
 
     if len(X_clipped) <= n_min:
         warnings.warn("# Does not have enough sky object: {0}".format(len(sigma)))
         return summary
 
     # Mean, median, and standard deviation
-    summary['mean'] = np.mean(X_clipped)
-    summary['std'] = np.std(X_clipped)
-    summary['median'] = np.median(X_clipped)
+    summary[keys[2]] = np.mean(X_clipped)
+    summary[keys[3]] = np.std(X_clipped)
+    summary[keys[4]] = np.median(X_clipped)
 
     if kde:
         if bw is None:
-            bw = 0.2 * summary['std']
-        summary['kde'] = gaussian_kde(X_clipped, bw_method=bw)
+            bw = 0.2 * summary[keys[4]]
+        summary[keys[5]] = gaussian_kde(X_clipped, bw_method=bw)
+
+    summary[keys[6]] = sigma
 
     return summary
