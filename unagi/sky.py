48444a1a0f48c28c64ad418c55e3b05ca7084bc4
dr_guangtou <dr.guangtou@gmail.com>
2019-05-16 01:39:25 -0700

    Keep working on the SkyObj class

--- unagi/sky.py
+++ unagi/sky.py
@@ -3,6 +3,8 @@
 # -*- coding: utf-8 -*-
 """Sky background related"""
 
+from __future__ import annotations
+
 import os
 import copy
 import warnings
@@ -12,7 +14,52 @@ from scipy.stats import sigmaclip
 
 from astropy.table import Table
 
-__all__ = ['SkyObjs']
+from . import utils
+
+__all__ = ['SkyObjs', 'AperPhot', 'S18A_APER']
+
+
+class AperPhot():
+    """
+    Class for aperture photometry in HSC.
+    """
+    PIX = 0.168 # arcsec/pixe
+
+    def __init__(self, name, rad, rerun='s18a'):
+        """Start a aperture photometry object."""
+        self.aper_id = name
+        self.r_pix = rad
+        self.area_pix = np.pi * (rad ** 2.0)
+        self.r_arcsec = rad * self.PIX
+        self.area_arcsec = np.pi * (self.r_arcsec ** 2.0)
+
+        if rerun == 's18a':
+            self.flux_col = self.s18a_flux()
+            self.err_col = self.s18a_err()
+        else:
+            raise NotImplementedError("# Only S18A data are available.")
+
+    def s18a_flux(self, band=None):
+        """Aperture flux column name in S18A."""
+        if band is not None:
+            return "{0}_apertureflux_{1}_flux".format(band.strip(), self.aper_id)
+
+        return "apertureflux_{0}_flux".format(self.aper_id)
+
+    def s18a_err(self, band=None):
+        """Aperture flux error column name in S18A."""
+        if band is not None:
+            return "{0}_{1}sigma".format(band.strip(), self.s18a_flux())
+
+        return "{0}sigma".format(self.s18a_flux())
+
+# Aperture flux in S18A
+S18A_APER_ID = ['10', '15', '20', '30', '40', '57', '84',
+                '118', '168', '235']
+S18A_APER_RAD = [3.0, 4.5, 6.0, 9.0, 12.0, 17.0, 25.0, 35.0, 50.0, 70.0]
+S18A_APER = {}
+for ii, rad in zip(S18A_APER_ID, S18A_APER_RAD):
+    S18A_APER['aper{0}'.format(ii)] = AperPhot(ii, rad)
 
 
 class SkyObjs():
@@ -23,10 +70,10 @@ class SkyObjs():
     CGS_TO_MUJY = 1.7378E30
 
     # List of filters
-    FILTER_LIST = ['HSC-G', 'HSC-R', 'HSC-I', 'HSC-Z', 'HSC-Y',
-                   'NB0387', 'NB0816', 'NB0921']
+    FILTER_LIST = ['HSC-G', 'HSC-R', 'HSC-I', 'HSC-Z', 'HSC-Y']
+
     # Nicknames of filters
-    FILTER_SHORT = ['g', 'r', 'i', 'z', 'y', 'nb0387', 'nb816', 'nb921']
+    FILTER_SHORT = ['g', 'r', 'i', 'z', 'y']
 
     def __init__(self, skyobjs, meas=False, nobj_min=5):
         """
@@ -70,11 +117,11 @@ class SkyObjs():
 
         # List of Patches and Tracts
         self.tract_patch = np.unique(
-            ["{0}_{1:03d}".format(t, p) for t, p in 
+            ["{0}_{1:03d}".format(t, p) for t, p in
              zip(self.skyobjs['tract'], self.skyobjs['patch'])])
         self.n_tract_patch = len(self.tract_patch)
 
-    def select_tract(self, tract, patch=None, n_min=10):
+    def select_tract(self, tract, patch=None, n_min=10) -> 'SkyObjs':
         """Select sky objects on one Tract (and Patch) from the catalog """
         tract_mask = self.skyobjs['tract'] == tract
         if tract_mask.sum() == 0:
@@ -93,9 +140,17 @@ class SkyObjs():
             warnings.warn("# Less than {0} skyobjs is available.")
             return None
 
-        return copy.deepcopy(self.skyobjs[tract_mask])
+        return SkyObjs(self.skyobjs[tract_mask])
 
-    def flux_stats(self, flux_col, sigma=5, to_mujy=True):
+    def select_box(self):
+        """Select sky objects in a box region."""
+        raise NotImplementedError("# Not yet")
+
+    def select_circle(self):
+        """Select sky objects within a circle."""
+        raise NotImplementedError("# Not yet")
+
+    def flux_stats(self, flux_col, sigma=3.5, kde=False, bw=None, to_mujy=True):
         """Basic statistics of the flux."""
         u_factor = self.CGS_TO_MUJY if to_mujy else 1.0
 
@@ -104,16 +159,23 @@ class SkyObjs():
         except ValueError:
             raise Exception("# Wrong flux column name: {0}".format(flux_col))
 
-        # Only use the ones with a good flux
-        mask = np.isfinite(flux)
-        if mask.sum() <= self.n_min:
-            warnings.warn("# Does not have enough sky object: {0}".format(mask.sum()))
-        flux_use = flux[mask]
+        return utils.stats_summary(flux, sigma=sigma, n_min=self.n_min,
+                                   kde=kde, bw=bw)
 
-        # Sigma clipping
-        if sigma is not None and sigma > 0:
-            flux_use, flux_low, flux_upp = sigmaclip(flux_use, low=sigma, high=sigma)
-        if len(flux_use) <= self.n_min:
-            warnings.warn("# Does not have enough sky object: {0}".format(len(sigma)))
+    def snr_stats(self, flux_col, err_col, sigma=3.5, kde=False, bw=None):
+        """Basic statistics of the S/N."""
+        try:
+            snr = self.skyobjs[flux_col] / self.skyobjs[err_col]
+        except ValueError:
+            raise Exception("# Wrong column names: {0}/{1}".format(flux_col, err_col))
 
-        return np.mean(flux_use), np.median(flux_use), np.std(flux_use)
+        return utils.stats_summary(snr, sigma=sigma, n_min=self.n_min,
+                                   kde=kde, bw=bw)
+
+    def mu_stats(self, aper):
+        """Basic statistics of the aperture flux density."""
+        raise NotImplementedError("# Not yet")
+    
+    def sum_all_filters(self, aper):
+        """Provide a summary of sky objects in all five bands.""" 
+        raise NotImplementedError("# Not yet")
