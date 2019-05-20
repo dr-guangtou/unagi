5140587e2965331b3d059baacea25193955d2dbf
dr-guangtou <dr.guangtou@gmail.com>
2019-05-20 16:29:27 -0700

    Place holder for HSC cutout

--- unagi/task.py
+++ unagi/task.py
@@ -13,7 +13,7 @@ from astropy.visualization import make_lupton_rgb
 from .hsc import Hsc
 from .utils import r_phy_to_ang
 
-__all__ = ['hsc_tricolor']
+__all__ = ['hsc_tricolor', 'hsc_cutout']
 
 ANG_UNITS = ['arcsec', 'arcsecond', 'arcmin', 'arcminute', 'deg']
 PHY_UNITS = ['pc', 'kpc', 'Mpc']
@@ -25,6 +25,107 @@ def hsc_tricolor(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
                  save_rgb=False, rgb_q=15, rgb_stretch=0.5, rgb_min=0):
     """
     Generate HSC 3-color picture using coadd image.
+    """
+    # Login to HSC archive
+    if archive is None:
+        archive = Hsc(dr=dr, rerun=rerun)
+
+    # List of three filters
+    filter_list = list(filters)
+    if len(filter_list) is not 3:
+        raise ValueError("# Need and only need three filters!")
+
+    # Check the choices of filters
+    assert np.all(
+        [(f in archive.FILTER_SHORT) or (f in archive.FILTER_LIST)
+         for f in filter_list]), '# Wrong filter choice!'
+
+    # Parse the cutout image size.
+    # We use central coordinate and half image size as the default format.
+    if not isinstance(cutout_size, u.quantity.Quantity):
+        if verbose:
+            print("# Assume the cutout size is in arcsec unit.")
+        cutout_size = cutout_size * u.Unit('arcsec')
+        ang_size = cutout_size
+    else:
+        cutout_unit = cutout_size.unit
+        if str(cutout_unit) in ANG_UNITS:
+            ang_size = cutout_size.to(u.Unit('arcsec'))
+        elif str(cutout_unit) in PHY_UNITS:
+            if redshift is None:
+                raise ValueError("# Need to provide redshift value to use physical size!")
+            elif (redshift < 0.) or (~np.isfinite(redshift)):
+                raise ValueError("# Redshift value is not valid!")
+            else:
+                ang_size = r_phy_to_ang(cutout_size, redshift, cosmo=cosmo)
+        else:
+            raise ValueError("# Wrong unit for cutout size: {}".format(str(cutout_unit)))
+
+    # Output file names
+    if prefix is None:
+        # TODO: More informed prefix
+        ra_str, dec_str = coord.to_string('decimal', precision=4).split(' ')
+        size_str = "{:8.2f}{}".format(cutout_size.value, cutout_size.unit).strip()
+        prefix = '{0}_{1}_{2}_{3}_{4}'.format(dr, rerun, ra_str, dec_str, size_str)
+
+    # List of fits file
+    fits_list = ['_'.join([prefix, f]) + '.fits' for f in filter_list]
+
+    # Availability of each file
+    fits_save = [os.path.isfile(f) or os.path.islink(f) for f in fits_list]
+
+    # Name of the output JPEG picture (if necessary)
+    if save_rgb:
+        # Generate a string that represents the filter combination
+        color_str = ''.join(
+            [f if f in archive.FILTER_SHORT else archive.FILTER_SHORT[archive.FILTER_LIST.index(f)]
+             for f in filter_list])
+        rgb_jpg = prefix + '_{0}.jpg'.format(color_str)
+        if verbose:
+            print("# RGB picture will be saved as {}".format(rgb_jpg))
+    else:
+        rgb_jpg = None
+
+    # List of RGB data
+    rgb_cube = []
+
+    # Load the cutout images in three bands
+    for ii, filt in enumerate(filter_list):
+        if fits_save[ii] and use_saved:
+            if verbose:
+                print("# Read in saved FITS file: {}".format(fits_save[ii]))
+            cutout_hdu = fits.open(fits_list[ii])
+        else:
+            if verbose:
+                print("# Retrieving cutout image in filter: {}".format(filt))
+            cutout_hdu = archive.get_cutout_image(
+                coord, w_half=ang_size, h_half=ang_size, filt=filt)
+            if save_img:
+                _ = cutout_hdu.writeto(fits_list[ii], overwrite=True)
+
+        if ii == 0:
+            cutout_wcs = wcs.WCS(cutout_hdu[hdu].header)
+
+        rgb_cube.append(cutout_hdu[hdu].data)
+
+    # If the filters are not in RGB order, reverse it
+    if not rgb_order:
+        rgb_cube.reverse()
+
+    cutout_rgb = make_lupton_rgb(rgb_cube[0], rgb_cube[1], rgb_cube[2],
+                                 Q=rgb_q, stretch=rgb_stretch, minimum=rgb_min,
+                                 filename=rgb_jpg)
+
+    return cutout_rgb, cutout_wcs
+
+
+def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
+               dr='dr2', rerun='s18a_wide', redshift=None, cosmo=None,
+               prefix=None, use_saved=False, save_img=False, verbose=True,
+               rgb_order=False, hdu=1, archive=None,
+               save_rgb=False, rgb_q=15, rgb_stretch=0.5, rgb_min=0):
+    """
+    Generate HSC cutout images.
     """
     # Login to HSC archive
     if archive is None:
