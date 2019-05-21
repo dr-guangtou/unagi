1991492f4c79f238ee10e0945b8fc866f80a2c49
dr-guangtou <dr.guangtou@gmail.com>
2019-05-20 22:13:40 -0700

    Add basic image displaying function to unagi

--- unagi/plotting.py
+++ unagi/plotting.py
@@ -4,9 +4,14 @@
 
 import numpy as np
 
+from astropy.visualization import ZScaleInterval, \
+    AsymmetricPercentileInterval
+
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
+from matplotlib import colors
 from matplotlib.colorbar import Colorbar
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
 plt.rc('text', usetex=True)
 rcParams.update({'axes.linewidth': 1.5})
@@ -29,8 +34,12 @@ rcParams.update({'ytick.minor.width': '1.5'})
 rcParams.update({'axes.titlepad': '10.0'})
 rcParams.update({'font.size': 25})
 
+__all__ = ['FILTERS_COLOR', 'plot_skyobj_hist', 'map_skyobjs', 'random_cmap',
+           'display_single']
 
-__all__ = ['FILTERS_COLOR', 'plot_skyobj_hist', 'map_skyobjs']
+# Default colormaps
+IMG_CMAP = plt.get_cmap('viridis')
+IMG_CMAP.set_bad(color='black')
 
 FILTERS_COLOR = ['#2ca02c', '#ff7f0e', '#d62728', '#8c564b', '#7f7f7f']
 FILTERS_SHORT = ['g', 'r', 'i', 'z', 'y']
@@ -146,3 +155,204 @@ def map_skyobjs(x, y, n, mu, label=None, n_min=10, vmin=None, vmax=None,
     _ = ax1.set_ylabel(r'$\mathrm{Dec\ [deg]}$', fontsize=fontsize)
 
     return fig
+
+def random_cmap(ncolors=256, background_color='white'):
+    """Random color maps.
+
+    Generate a matplotlib colormap consisting of random (muted) colors.
+    A random colormap is very useful for plotting segmentation images.
+
+    Parameters
+    ----------
+    ncolors : int, optional
+        The number of colors in the colormap.  The default is 256.
+    random_state : int or `~numpy.random.RandomState`, optional
+        The pseudo-random number generator state used for random
+        sampling.  Separate function calls with the same
+        ``random_state`` will generate the same colormap.
+
+    Returns
+    -------
+    cmap : `matplotlib.colors.Colormap`
+        The matplotlib colormap with random colors.
+
+    Notes
+    -----
+    Based on: colormaps.py in photutils
+
+    """
+    prng = np.random.mtrand._rand
+
+    h = prng.uniform(low=0.0, high=1.0, size=ncolors)
+    s = prng.uniform(low=0.2, high=0.7, size=ncolors)
+    v = prng.uniform(low=0.5, high=1.0, size=ncolors)
+
+    hsv = np.dstack((h, s, v))
+    rgb = np.squeeze(colors.hsv_to_rgb(hsv))
+
+    if background_color is not None:
+        if background_color not in colors.cnames:
+            raise ValueError('"{0}" is not a valid background color '
+                             'name'.format(background_color))
+        rgb[0] = colors.hex2color(colors.cnames[background_color])
+
+    return colors.ListedColormap(rgb)
+
+def display_single(img,
+                   pixel_scale=0.168,
+                   physical_scale=None,
+                   xsize=8,
+                   ysize=8,
+                   ax=None,
+                   stretch='arcsinh',
+                   scale='zscale',
+                   contrast=0.25,
+                   no_negative=False,
+                   lower_percentile=1.0,
+                   upper_percentile=99.0,
+                   cmap=IMG_CMAP,
+                   scale_bar=True,
+                   scale_bar_length=5.0,
+                   scale_bar_fontsize=20,
+                   scale_bar_y_offset=0.5,
+                   scale_bar_color='w',
+                   scale_bar_loc='left',
+                   color_bar=False,
+                   color_bar_loc=1,
+                   color_bar_width='75%',
+                   color_bar_height='5%',
+                   color_bar_fontsize=18,
+                   color_bar_color='w',
+                   add_text=None,
+                   text_fontsize=30,
+                   text_color='w'):
+    """Display single image.
+
+    Parameters
+    ----------
+        img: np 2-D array for image
+
+        xsize: int, default = 8
+            Width of the image.
+
+        ysize: int, default = 8
+            Height of the image.
+
+    """
+    if ax is None:
+        fig = plt.figure(figsize=(xsize, ysize))
+        ax1 = fig.add_subplot(111)
+    else:
+        ax1 = ax
+
+    # Stretch option
+    if stretch.strip() == 'arcsinh':
+        img_scale = np.arcsinh(img)
+    elif stretch.strip() == 'log':
+        if no_negative:
+            img[img <= 0.0] = 1.0E-10
+        img_scale = np.log(img)
+    elif stretch.strip() == 'log10':
+        if no_negative:
+            img[img <= 0.0] = 1.0E-10
+        img_scale = np.log10(img)
+    elif stretch.strip() == 'linear':
+        img_scale = img
+    else:
+        raise Exception("# Wrong stretch option.")
+
+    # Scale option
+    if scale.strip() == 'zscale':
+        try:
+            zmin, zmax = ZScaleInterval(contrast=contrast).get_limits(img_scale)
+        except IndexError:
+            # TODO: Deal with problematic image
+            zmin, zmax = -1.0, 1.0
+    elif scale.strip() == 'percentile':
+        try:
+            zmin, zmax = AsymmetricPercentileInterval(
+                lower_percentile=lower_percentile,
+                upper_percentile=upper_percentile).get_limits(img_scale)
+        except IndexError:
+            # TODO: Deal with problematic image
+            zmin, zmax = -1.0, 1.0
+    else:
+        zmin, zmax = np.nanmin(img_scale), np.nanmax(img_scale)
+
+    show = ax1.imshow(img_scale, origin='lower', cmap=cmap,
+                      vmin=zmin, vmax=zmax)
+
+    # Hide ticks and tick labels
+    ax1.tick_params(
+        labelbottom=False,
+        labelleft=False,
+        axis=u'both',
+        which=u'both',
+        length=0)
+
+    # Put scale bar on the image
+    (img_size_x, img_size_y) = img.shape
+    if physical_scale is not None:
+        pixel_scale *= physical_scale
+    if scale_bar:
+        if scale_bar_loc == 'left':
+            scale_bar_x_0 = int(img_size_x * 0.04)
+            scale_bar_x_1 = int(img_size_x * 0.04 +
+                                (scale_bar_length / pixel_scale))
+        else:
+            scale_bar_x_0 = int(img_size_x * 0.95 -
+                                (scale_bar_length / pixel_scale))
+            scale_bar_x_1 = int(img_size_x * 0.95)
+
+        scale_bar_y = int(img_size_y * 0.10)
+        scale_bar_text_x = (scale_bar_x_0 + scale_bar_x_1) / 2
+        scale_bar_text_y = (scale_bar_y * scale_bar_y_offset)
+        if physical_scale is not None:
+            scale_bar_text = r'$%d\ \mathrm{kpc}$' % int(scale_bar_length)
+        else:
+            scale_bar_text = r'$%d^{\prime\prime}$' % int(scale_bar_length)
+        scale_bar_text_size = scale_bar_fontsize
+
+        ax1.plot(
+            [scale_bar_x_0, scale_bar_x_1], [scale_bar_y, scale_bar_y],
+            linewidth=3,
+            c=scale_bar_color,
+            alpha=1.0)
+        ax1.text(
+            scale_bar_text_x,
+            scale_bar_text_y,
+            scale_bar_text,
+            fontsize=scale_bar_text_size,
+            horizontalalignment='center',
+            color=scale_bar_color)
+    if add_text is not None:
+        text_x_0 = int(img_size_x*0.08)
+        text_y_0 = int(img_size_y*0.80)
+        ax1.text(
+            text_x_0, text_y_0, r'$\mathrm{'+add_text+'}$', 
+            fontsize=text_fontsize, color=text_color)
+
+    # Put a color bar on the image
+    if color_bar:
+        ax_cbar = inset_axes(ax1,
+                             width=color_bar_width,
+                             height=color_bar_height,
+                             loc=color_bar_loc)
+        if ax is None:
+            cbar = plt.colorbar(show, ax=ax1, cax=ax_cbar,
+                                orientation='horizontal')
+        else:
+            cbar = plt.colorbar(show, ax=ax, cax=ax_cbar,
+                                orientation='horizontal')
+
+        cbar.ax.xaxis.set_tick_params(color=color_bar_color)
+        cbar.ax.yaxis.set_tick_params(color=color_bar_color)
+        cbar.outline.set_edgecolor(color_bar_color)
+        plt.setp(plt.getp(cbar.ax.axes, 'xticklabels'),
+                 color=color_bar_color, fontsize=color_bar_fontsize)
+        plt.setp(plt.getp(cbar.ax.axes, 'yticklabels'),
+                 color=color_bar_color, fontsize=color_bar_fontsize)
+
+    if ax is None:
+        return fig
+    return ax1
