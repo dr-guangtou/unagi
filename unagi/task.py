b8b7af0e5b7b9fc250b02060b3131f5821003119
dr-guangtou <dr.guangtou@gmail.com>
2019-05-20 17:13:15 -0700

    Start to work on the hsc_cutout task

--- unagi/task.py
+++ unagi/task.py
@@ -119,11 +119,9 @@ def hsc_tricolor(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
     return cutout_rgb, cutout_wcs
 
 
-def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
-               dr='dr2', rerun='s18a_wide', redshift=None, cosmo=None,
-               prefix=None, use_saved=False, save_img=False, verbose=True,
-               rgb_order=False, hdu=1, archive=None,
-               save_rgb=False, rgb_q=15, rgb_stretch=0.5, rgb_min=0):
+def hsc_cutout(coord, coord_2=None, cutout_size=10.0 * u.Unit('arcsec'), filters='i',
+               dr='dr2', rerun='s18a_wide', redshift=None, cosmo=None, use_saved=True,
+               prefix=None, verbose=True, archive=None, save_fits=True, **kwargs):
     """
     Generate HSC cutout images.
     """
@@ -133,8 +131,8 @@ def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
 
     # List of three filters
     filter_list = list(filters)
-    if len(filter_list) is not 3:
-        raise ValueError("# Need and only need three filters!")
+    if len(filter_list) > 1 and verbose:
+        print("# Will dgenerate cutouts for a list of filters:", filter_list)
 
     # Check the choices of filters
     assert np.all(
@@ -143,28 +141,30 @@ def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
 
     # Parse the cutout image size.
     # We use central coordinate and half image size as the default format.
-    if not isinstance(cutout_size, u.quantity.Quantity):
-        if verbose:
-            print("# Assume the cutout size is in arcsec unit.")
-        cutout_size = cutout_size * u.Unit('arcsec')
-        ang_size = cutout_size
-    else:
-        cutout_unit = cutout_size.unit
-        if str(cutout_unit) in ANG_UNITS:
-            ang_size = cutout_size.to(u.Unit('arcsec'))
-        elif str(cutout_unit) in PHY_UNITS:
-            if redshift is None:
-                raise ValueError("# Need to provide redshift value to use physical size!")
-            elif (redshift < 0.) or (~np.isfinite(redshift)):
-                raise ValueError("# Redshift value is not valid!")
-            else:
-                ang_size = r_phy_to_ang(cutout_size, redshift, cosmo=cosmo)
+    if coord_2 is None:
+        if not isinstance(cutout_size, u.quantity.Quantity):
+            if verbose:
+                print("# Assume the cutout size is in arcsec unit.")
+            cutout_size = cutout_size * u.Unit('arcsec')
+            ang_size = cutout_size
         else:
-            raise ValueError("# Wrong unit for cutout size: {}".format(str(cutout_unit)))
+            cutout_unit = cutout_size.unit
+            if str(cutout_unit) in ANG_UNITS:
+                ang_size = cutout_size.to(u.Unit('arcsec'))
+            elif str(cutout_unit) in PHY_UNITS:
+                if redshift is None:
+                    raise ValueError("# Need to provide redshift value to use physical size!")
+                elif (redshift < 0.) or (~np.isfinite(redshift)):
+                    raise ValueError("# Redshift value is not valid!")
+                else:
+                    ang_size = r_phy_to_ang(cutout_size, redshift, cosmo=cosmo)
+            else:
+                raise ValueError("# Wrong unit for cutout size: {}".format(str(cutout_unit)))
+    else:
+        ang_size = None
 
     # Output file names
     if prefix is None:
-        # TODO: More informed prefix
         ra_str, dec_str = coord.to_string('decimal', precision=4).split(' ')
         size_str = "{:8.2f}{}".format(cutout_size.value, cutout_size.unit).strip()
         prefix = '{0}_{1}_{2}_{3}_{4}'.format(dr, rerun, ra_str, dec_str, size_str)
@@ -175,22 +175,9 @@ def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
     # Availability of each file
     fits_save = [os.path.isfile(f) or os.path.islink(f) for f in fits_list]
 
-    # Name of the output JPEG picture (if necessary)
-    if save_rgb:
-        # Generate a string that represents the filter combination
-        color_str = ''.join(
-            [f if f in archive.FILTER_SHORT else archive.FILTER_SHORT[archive.FILTER_LIST.index(f)]
-             for f in filter_list])
-        rgb_jpg = prefix + '_{0}.jpg'.format(color_str)
-        if verbose:
-            print("# RGB picture will be saved as {}".format(rgb_jpg))
-    else:
-        rgb_jpg = None
+    # Get the cutout in each band
+    cutout_list = []
 
-    # List of RGB data
-    rgb_cube = []
-
-    # Load the cutout images in three bands
     for ii, filt in enumerate(filter_list):
         if fits_save[ii] and use_saved:
             if verbose:
@@ -199,22 +186,15 @@ def hsc_cutout(coord, cutout_size=10.0 * u.Unit('arcsec'), filters='gri',
         else:
             if verbose:
                 print("# Retrieving cutout image in filter: {}".format(filt))
-            cutout_hdu = archive.get_cutout_image(
-                coord, w_half=ang_size, h_half=ang_size, filt=filt)
-            if save_img:
-                _ = cutout_hdu.writeto(fits_list[ii], overwrite=True)
+            if save_fits:
+                cutout_hdu = archive.download_cutout(
+                    coord, fits_list[ii], coord_2=coord_2, w_half=ang_size, h_half=ang_size,
+                    filt=filt, **kwargs)
+            else:
+                cutout_hdu = archive.get_cutout_image(
+                    coord, coord_2=coord_2, w_half=ang_size, h_half=ang_size, filt=filt, **kwargs)
 
-        if ii == 0:
-            cutout_wcs = wcs.WCS(cutout_hdu[hdu].header)
+        # Append the HDU to the list
+        cutout_list.append(cutout_hdu)
 
-        rgb_cube.append(cutout_hdu[hdu].data)
-
-    # If the filters are not in RGB order, reverse it
-    if not rgb_order:
-        rgb_cube.reverse()
-
-    cutout_rgb = make_lupton_rgb(rgb_cube[0], rgb_cube[1], rgb_cube[2],
-                                 Q=rgb_q, stretch=rgb_stretch, minimum=rgb_min,
-                                 filename=rgb_jpg)
-
-    return cutout_rgb, cutout_wcs
+    return cutout_list
