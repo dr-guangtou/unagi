279ff04ac33cda5c0cd8b0ed5a0d90e0d82b998f
dr-guangtou <dr.guangtou@gmail.com>
2019-06-03 15:47:58 -0700

    Update the example query to PDR2

--- unagi/test/test_query_1.sql
+++ unagi/test/test_query_1.sql
@@ -1,21 +1,22 @@
-SELECT 
-        object_id 
-      , ra 
-      , dec 
-      , imag_kron 
-      , imag_kron_err 
-      , ymag_kron 
-      , ymag_kron_err 
-      , imag_kron - ymag_kron AS i_y 
-    FROM 
-        pdr1_udeep.forced 
-    WHERE 
-        boxsearch(coord, 34.0, 36.0, -5.0, -4.5) 
-        /* is equivalent to 
-             ra BETWEEN 34.0 AND 36.0 
-             AND dec BETWEEN -5.0 AND -4.5 
-           but boxSearch() is much faster 
-        */ 
-        AND imag_kron < 25.5 
+SELECT
+	object_id
+      , ra
+      , dec
+      , i_kronflux_mag
+      , i_kronflux_magsigma
+      , y_kronflux_mag
+      , y_kronflux_magsigma
+      , i_kronflux_mag - y_kronflux_mag AS i_y
+    FROM
+	pdr2_dud.forced
+	JOIN pdr2_dud.forced2 USING (object_id)
+    WHERE
+	  boxSearch(coord, 34.0, 36.0, -5.0, -4.5)
+          /* is equivalent to
+                 ra  BETWEEN 34.0 AND 36.0
+             AND dec BETWEEN -5.0 AND -4.5
+             but boxSearch() is much faster
+          */
+	AND i_kronflux_mag < 25.5
     LIMIT 10
 ;
