7d3e0c9d3a3d610b308f743a49f7c4cfbd72cb43
dr-guangtou <dr.guangtou@gmail.com>
2019-06-10 14:39:05 -0700

    Add function to convert world coordinate to image X, Y coordinate

--- unagi/catalog.py
+++ unagi/catalog.py
@@ -4,8 +4,38 @@
 
 import numpy as np
 
-__all__ = ['moments_to_shape']
+import astropy.units as u
+from astropy.table import Column
 
+__all__ = ['moments_to_shape', 'abmag_to_image', 'world_to_image']
+
+# Flux unit in HSC catalog
+FLUX_UNIT_S16A = (u.erg / u.s / u.Hz / u.cm ** 2)
+FLUX_UNIT_S17A = (u.erg / u.s / u.Hz / u.cm ** 2)
+FLUX_UNIT_S18A = (u.erg / u.s / u.Hz / u.cm ** 2)
+FLUX_UNIT_PDR1 = (u.Jansky * 1.E-9)
+FLUX_UNIT_PDR2 = (u.Jansky * 1.E-9)
+
+def abmag_to_image(abmag):
+    """
+    Convert AB magnitude into HSC image flux unit.
+    """
+    return 10.0 ** ((27.0 - abmag) / 2.5)
+
+def world_to_image(catalog, wcs, ra='ra', dec='dec', update=True):
+    """
+    Get the X, Y coordinate on the image.
+    """
+    xy_arr = np.asarray(
+        [wcs.all_world2pix(catalog[ra][ii], catalog[dec][ii], 0)
+         for ii in np.arange(len(catalog))])
+    x_arr, y_arr = xy_arr[:, 0], xy_arr[:, 1]
+
+    if update:
+        catalog.add_column(Column(data=x_arr, name='x'))
+        catalog.add_column(Column(data=y_arr, name='y'))
+        return catalog
+    return x_arr, y_arr
 
 def moments_to_shape(xx, yy, xy, axis_ratio=False, radian=False):
     """
